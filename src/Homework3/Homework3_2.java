8a52fb736406b8ba055c719da51c8f2c8350d7bd
wctsai <shinobumw@users.noreply.github.com>
2023-09-15 17:18:58 +0800

    Finish the advanced part of Homework3_2

--- src/Homework3/Homework3_2.java
+++ src/Homework3/Homework3_2.java
@@ -1,21 +1,21 @@
 package Homework3;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Homework3_2 {
     public static void main(String[] args) {
-        System.out.println("開始猜數字吧！");
-        int number = (int)(Math.random() * 10);
-        Scanner s = new Scanner(System.in);
-        while (true) {
-            int input = s.nextInt();
-            if (input == number) {
+        System.out.println("開始猜數字吧！請輸入一個整數介於0~100：");
+        int number = (int)(Math.random() * 101);
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            int input = scanner.nextInt();
+            if (input > number) {
+                System.out.println("猜錯囉，請再輸入小一點的數字");
+            } else if (input < number) {
+                System.out.println("猜錯囉，請再輸入大一點的數字");
+            } else {
                 System.out.println("答對了！答案就是" + number);
                 break;
-            } else {
-                System.out.println("猜錯囉");
-                continue;
             }
         }
     }
